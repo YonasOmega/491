7b91429caf84238ec3d07a80ececbb7eda75d249
yonasomega <yonas123@uw.edu>
2025-01-26 15:22:48 -0800

    sus

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./Paladin_Walking_Centered.png")
+ASSET_MANAGER.queueDownload("./image.png")
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
 
-	gameEngine.addEntity(new Paladin(gameEngine));
+	gameEngine.addEntity(new Suspect(gameEngine));
 
 	gameEngine.init(ctx);
 
